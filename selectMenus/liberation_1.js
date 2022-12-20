705f706188f4e19e544e15e66c372b26c78920fe
Hinorpio_DESKTOP <hinscorpio@gmail.com>
2022-12-21 02:38:06 +0800

    Update: Liberate

--- selectMenus/liberation_1.js
+++ selectMenus/liberation_1.js
@@ -9,8 +9,12 @@ const data = {
         "color": 0xF45F34,
         "fields": [
             {
-                "name": `第一階段`,
-                "value": `**必殺技，一星被動強化**`
+                "name": `<a:119:882828927842795530>**注意**<a:119:882828927842795530>`,
+                "value": `**由於角色眾多，故此解放所需素材中的對應角色的記憶精碎將統一以【無名的記憶碎片】的表情符號【<:item_nameless:1054798367210872843>】作顯示。如發生任何誤會，本喵並不會負責！**\n\n--------------`
+            },
+            {
+                "name": `<:liberate_1:1054796644136263731> 第一階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x20\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x300,000`
             },
             {
                 "name": `<:detail_skill:933339968967933963> 烈焰攻心`,
@@ -21,16 +25,16 @@ const data = {
                 "value": `使自身普攻傷害增加25%\n使自身必殺技傷害增加15%`
             },
             {
-                "name": `第二階段`,
-                "value": `**HP與ATK將永久提升10%**`
+                "name": `<:liberate_2:1054796646728355851> 第二階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:crystal_fire:1054638743249748099> 緋紅結晶x20\n<:item_fragment:1054638756210163812> 技之碎片x150，<:item_sliver:1054655504040865832> 銀幣x400,000`
             },
             {
                 "name": `<:5flower:981031925168865290> 完全體數據`,
-                "value": `HP：3,440,520 | ATK：910,400`
+                "value": `**HP與ATK將永久提升10%**\nHP：3,440,520 | ATK：910,400`
             },
             {
-                "name": `第三階段`,
-                "value": `**隊長技，三星被動，五星被動強化**`
+                "name": `<:liberate_3:1054796649035214879> 第三階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x10\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x500,000`
             },
             {
                 "name": `<:detail_leader:933339992732876811> 魔王巴爾之狡詐`,
@@ -49,7 +53,8 @@ const data = {
             "url": `https://cdn.discordapp.com/emojis/856492803358326804.webp?size=96&quality=lossless`,
             "height": 0,
             "width": 0
-        }
+        },
+        "imageUrl": "https://cdn.discordapp.com/attachments/933707595301150740/1054811284283064392/char-01-2e94cdaa4f11a45635703144d76d06bc.png"
     },
     'satan': {
         "type": "rich",
@@ -58,8 +63,12 @@ const data = {
         "color": 0xC390FF,
         "fields": [
             {
-                "name": `第一階段`,
-                "value": `**必殺技，普攻，一星被動強化**`
+                "name": `<a:119:882828927842795530>**注意**<a:119:882828927842795530>`,
+                "value": `**由於角色眾多，故此解放所需素材中的對應角色的記憶精碎將統一以【無名的記憶碎片】的表情符號【<:item_nameless:1054798367210872843>】作顯示。如發生任何誤會，本喵並不會負責！**\n\n--------------`
+            },
+            {
+                "name": `<:liberate_1:1054796644136263731> 第一階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x20\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x300,000`
             },
             {
                 "name": `<:detail_skill:933339968967933963> 暴戾恣睢`,
@@ -74,16 +83,16 @@ const data = {
                 "value": `使自身受到傷害減少20%`
             },
             {
-                "name": `第二階段`,
-                "value": `**HP與ATK將永久提升10%**`
+                "name": `<:liberate_2:1054796646728355851> 第二階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:crystal_dark:1054638740112408596> 靛紫結晶x20\n<:item_fragment:1054638756210163812> 技之碎片x150，<:item_sliver:1054655504040865832> 銀幣x400,000`
             },
             {
                 "name": `<:5flower:981031925168865290> 完全體數據`,
-                "value": `HP：3,440,520 | ATK：910,400`
+                "value": `**HP與ATK將永久提升10%**\nHP：4,947,826 | ATK：633,319`
             },
             {
-                "name": `第三階段`,
-                "value": `**隊長技，三星被動，五星被動強化**`
+                "name": `<:liberate_3:1054796649035214879> 第三階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x10\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x500,000`
             },
             {
                 "name": `<:detail_leader:933339992732876811> 魔王撒旦之狂妄`,
@@ -102,7 +111,8 @@ const data = {
             "url": `https://cdn.discordapp.com/emojis/856202436619403294.webp?size=96&quality=lossless`,
             "height": 0,
             "width": 0
-        }
+        },
+        "imageUrl": "https://cdn.discordapp.com/attachments/933707595301150740/1054811284631199837/char-02-62ba2a893efff53c60042743e5341b63.png"
     },
     'iblis': {
         "type": "rich",
@@ -111,8 +121,12 @@ const data = {
         "color": 0xEFB26B,
         "fields": [
             {
-                "name": `第一階段`,
-                "value": `**必殺技，一星被動強化**`
+                "name": `<a:119:882828927842795530>**注意**<a:119:882828927842795530>`,
+                "value": `**由於角色眾多，故此解放所需素材中的對應角色的記憶精碎將統一以【無名的記憶碎片】的表情符號【<:item_nameless:1054798367210872843>】作顯示。如發生任何誤會，本喵並不會負責！**\n\n--------------`
+            },
+            {
+                "name": `<:liberate_1:1054796644136263731> 第一階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x20\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x300,000`
             },
             {
                 "name": `<:detail_skill:933339968967933963> 劇毒之薔薇`,
@@ -123,16 +137,16 @@ const data = {
                 "value": `當前HP大於75％時，發動「造成傷害增加20%」`
             },
             {
-                "name": `第二階段`,
-                "value": `**HP與ATK將永久提升10%**`
+                "name": `<:liberate_2:1054796646728355851> 第二階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:crystal_light:1054638745816682556> 雪白結晶x20\n<:item_fragment:1054638756210163812> 技之碎片x150，<:item_sliver:1054655504040865832> 銀幣x400,000`
             },
             {
                 "name": `<:5flower:981031925168865290> 完全體數據`,
                 "value": `HP：3,166,609 | ATK：989,565`
             },
             {
-                "name": `第三階段`,
-                "value": `**隊長技，三星被動，五星被動強化**`
+                "name": `<:liberate_3:1054796649035214879> 第三階段`,
+                "value": `<:item_liberate:1054638758164705380> 解放晶石x1，<:item_nameless:1054798367210872843> 角色記憶精碎x10\n<:item_flawless:1054638754243027004> 無暇魂魄x1，<:item_sliver:1054655504040865832> 銀幣x500,000`
             },
             {
                 "name": `<:detail_leader:933339992732876811> 魔王伊布力斯之傲慢`,
@@ -151,7 +165,8 @@ const data = {
             "url": `https://cdn.discordapp.com/emojis/856202436821254184.webp?size=96&quality=lossless`,
             "height": 0,
             "width": 0
-        }
+        },
+        "imageUrl": "https://cdn.discordapp.com/attachments/933707595301150740/1054811285008699413/char-03-4a1ae928679be2f9258ef2637f7e9fe7.png"
     },
 }
 
@@ -167,8 +182,8 @@ module.exports = {
             .setColor(character.color)
             .setTitle(character.title)
             .setDescription(character.description)
-            .setThumbnail(character.thumbnail.url)
-            .addFields(character.fields);
+            .addFields(character.fields)
+            .setImage(character.imageUrl);
         await interaction.reply({
             content: `<@!${interaction.user.id}> ${nanascript}`,
             embeds: [embed],
